4cb698ccb08f477f2820ae41fc4ea1f3cec3f14b
Nadya <68276791+Nadya843@users.noreply.github.com>
2026-03-11 16:54:02 +0300

    Create Game class skeleton and public interface

--- MazeOfWords/include/Game.h
+++ MazeOfWords/include/Game.h
@@ -1,18 +1,30 @@
 #pragma once
 
+#include "Entity.h"
 #include "Maze.h"
 #include "WordManager.h"
-#include "Entity.h"
 
-#include <vector>
+#include <optional>
 #include <string>
+#include <vector>
 
 class Game {
     Maze maze_;
     Player player_{1, 1};
+    std::vector<Hint> hints_;
+    std::optional<FinalDoor> finalDoor_;
     WordManager wordManager_;
+
+    std::string targetWord_;
+    std::vector<bool> revealedLetters_;
+    bool gameOver_ = false;
+    bool win_ = false;
+    std::string statusMessage_;
+    int stepCounter_ = 0;
+    int scoreMultiplier_ = 1;
 
 public:
     explicit Game(const std::vector<std::string>& words);
-    void run();
+    int handleKey(int key);
+    void run(bool& restartFlag, bool& exitFlag);
 };
