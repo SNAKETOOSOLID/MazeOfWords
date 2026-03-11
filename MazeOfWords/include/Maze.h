c022cd291c2d3ec84b7aa10fd5f3f0db32597ed1
SNAKESSS <163165612+SNAKETOOSOLID@users.noreply.github.com>
2026-03-11 18:42:20 +0300

    extend maze header with frontier generation helpers

--- MazeOfWords/include/Maze.h
+++ MazeOfWords/include/Maze.h
@@ -6,6 +6,18 @@
 class Maze {
     std::vector<std::vector<bool>> cells_;
 
+    struct Cell {
+        int x;
+        int y;
+    };
+
+    bool inBoundsCell(int x, int y) const;
+
+    void addFrontier(int x, int y,
+                     std::vector<Cell>& frontier,
+                     std::vector<std::vector<bool>>& inFrontier,
+                     const std::vector<std::vector<bool>>& visited);
+
 public:
     Maze();
 
