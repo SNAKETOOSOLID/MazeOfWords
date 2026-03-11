e48f6704fb0436ea0340b121204921e5e3930657
NoquitNoway <raevskiyn2008@gmail.com>
2026-03-12 00:39:26 +0300

    Implemented multiplication question logic

--- MazeOfWords/include/MultiplicationQuestion.h
+++ MazeOfWords/include/MultiplicationQuestion.h
@@ -1,10 +1,27 @@
 #pragma once
 
+#include <random>
 #include <string>
 
 class MultiplicationQuestion {
+    int a_;
+    int b_;
+    int result_;
+
 public:
-    MultiplicationQuestion();
-    std::string prompt() const;
-    bool check(int answer) const;
+    MultiplicationQuestion() {
+        static std::mt19937 gen(std::random_device{}());
+        std::uniform_int_distribution<> dis(2, 9);
+        a_ = dis(gen);
+        b_ = dis(gen);
+        result_ = a_ * b_;
+    }
+
+    std::string prompt() const {
+        return std::to_string(a_) + " * " + std::to_string(b_) + " = ?";
+    }
+
+    bool check(int answer) const {
+        return answer == result_;
+    }
 };
