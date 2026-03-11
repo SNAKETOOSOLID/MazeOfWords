41bd32da7bf4f58f5a8453a22ee3adb6628eecfb
ToshiR-R <lera.01.24@yandex.ru>
2026-03-11 15:20:23 +0300

    generating repeated strings
    вспомогательная функция для форматирования вывода в консоли

--- MazeOfWords/src/ConsoleUtils.cpp
+++ MazeOfWords/src/ConsoleUtils.cpp
@@ -6,3 +6,10 @@ void setColor(WORD color) {
     SetConsoleTextAttribute(hConsole, color);
 }
 
+std::string repeatText(const std::string& s, int count) {
+    std::string result;
+    for (int i = 0; i < count; ++i) {
+        result += s;
+    }
+    return result;
+}
