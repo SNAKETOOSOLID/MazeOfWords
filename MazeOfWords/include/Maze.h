fe10ebf299fead8fd4c7abc7ef5209c7d37ad914
Nadya <68276791+Nadya843@users.noreply.github.com>
2026-03-13 03:09:31 +0300

    beauty fix

--- MazeOfWords/include/Maze.h
+++ MazeOfWords/include/Maze.h
@@ -13,14 +13,12 @@ class Maze {
 
     bool inBoundsCell(int x, int y) const;
 
-    void addFrontier(int x, int y,
-                     std::vector<Cell>& frontier,
+    void addFrontier(int x, int y, std::vector<Cell>& frontier,
                      std::vector<std::vector<bool>>& inFrontier,
                      const std::vector<std::vector<bool>>& visited);
 
 public:
     Maze();
-
     void generate();
     bool isPassable(int x, int y) const;
     std::vector<std::pair<int, int>> getPassableCells() const;
