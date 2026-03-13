e4b1d3b310bcbfeeddc4c323fbdecaed3dd0db14
Nadya <68276791+Nadya843@users.noreply.github.com>
2026-03-13 03:06:57 +0300

    add check на этапе компиляции(add constexpr)

--- MazeOfWords/include/Config.h
+++ MazeOfWords/include/Config.h
@@ -5,3 +5,7 @@ constexpr int MAZE_HEIGHT = 13;
 constexpr int NUM_HINTS = 6;
 constexpr int MAX_SCORE = 1500;
 constexpr int STEP_PENALTY = 5;
+constexpr bool isMazeSizeValid() {
+    return MAZE_WIDTH % 2 == 1 && MAZE_HEIGHT % 2 == 1;
+}
+static_assert(isMazeSizeValid(), "Maze dimensions must be odd");
